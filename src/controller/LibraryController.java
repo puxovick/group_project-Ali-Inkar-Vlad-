70494bdfe7131812528949ad2d8ec3e8c36116f9
abaytleuov90-ai <abaytleuov90@gmail.com>
2026-01-18 21:26:17 +0500

    Update LibraryController.java

--- src/controller/LibraryController.java
+++ src/controller/LibraryController.java
@@ -1,6 +1,40 @@
 package controller;
-
+import entity.Book;
+import repository.BookRepository;
+import repository.BorrowingRepository;
 public class LibraryController {
+    private BookRepository bookRepo = new BookRepository();
+        private BorrowingRepository borrowRepo = new BorrowingRepository();
 
+        public void borrowBook(int userId, int bookId) throws Exception {
+            Book book = bookRepo.findById(bookId);
 
-}
+            if (book == null) {
+            System.out.println("Book not found");
+            return;
+            }
+
+            if (book.getAvailableCopies() == 0) {
+            System.out.println("No copies available");
+            return;
+            }
+
+            borrowRepo.borrowBook(userId, bookId);
+            bookRepo.updateAvailableCopies(bookId, book.getAvailableCopies() - 1);
+
+            System.out.println("Book borrowed successfully");
+        }
+
+        public void returnBook(int borrowingId, int bookId) throws Exception {
+            borrowRepo.returnBook(borrowingId);
+            Book book = bookRepo.findById(bookId);
+
+            bookRepo.updateAvailableCopies(bookId, book.getAvailableCopies() + 1);
+
+            System.out.println("Book returned successfully");
+        }
+
+        public void showBorrowedBooks() throws Exception {
+            borrowRepo.showBorrowedBooks();
+        }
+    }

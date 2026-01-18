2fa632ce4b5dfef29afa0a40ff68e26a695eee86
abaytleuov90-ai <abaytleuov90@gmail.com>
2026-01-18 21:24:21 +0500

    Update DBConnection.java

--- src/config/DBConnection.java
+++ src/config/DBConnection.java
@@ -1,7 +1,13 @@
-
-
+package config;
+import java.sql.Connection;
+import java.sql.DriverManager;
 public class DBConnection {
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/library_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "0000";
 
-
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
-

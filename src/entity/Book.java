63d6374344294ffdb7388d79ef629e3621ef5eff
Ingkar <malikinkar@icloud.com>
2026-01-18 21:21:49 +0500

    Add constructor and getter for availableCopies in Book

--- src/entity/Book.java
+++ src/entity/Book.java
@@ -1,5 +1,17 @@
 package entity;
 
 public class Book {
+    private int id;
+    private String title;
+    private int availableCopies;
 
+    public Book(int id, String title, int availableCopies) {
+        this.id = id;
+        this.title = title;
+        this.availableCopies = availableCopies;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
 }
